7d4bdd643e74e97dd34a0735493308aba3153c16
Joshua Hill <98056388+Joshx27@users.noreply.github.com>
2023-11-08 14:02:48 -0600

    updated radix sort

--- parallel-based-sorting-algorithms/src/test/java/michaeljosh/radixSortTest.java
+++ parallel-based-sorting-algorithms/src/test/java/michaeljosh/radixSortTest.java
@@ -1,5 +1,18 @@
 package michaeljosh;
 
-public class radixSortTest {
+import static org.junit.Assert.assertArrayEquals;
 
+import org.junit.Test;
+
+public class radixSortTest {
+    @Test
+    public void testRadixSort() {
+        int[] unsortedArray = { 4, 5, 6, 7, 3, 1, 8, 2, 9, 10 };
+        int[] sortedArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        radixSort.radixSort(unsortedArray); // Assuming you have a Mergesort class with a static sort method
+
+        // Assert that the sorted array matches the expected result
+        assertArrayEquals(sortedArray, unsortedArray);
+    }
 }
