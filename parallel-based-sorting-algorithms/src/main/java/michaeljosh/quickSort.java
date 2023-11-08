ff512cd767bce08d70eb440f396c90d8e6d4f89a
Joshua Hill <98056388+Joshx27@users.noreply.github.com>
2023-11-08 13:55:50 -0600

    update quicksort

--- parallel-based-sorting-algorithms/src/main/java/michaeljosh/quickSort.java
+++ parallel-based-sorting-algorithms/src/main/java/michaeljosh/quickSort.java
@@ -37,25 +37,4 @@ public class quickSort {
 
         return i + 1;
     }
-
-    // A utility function to print array of size n
-    static void printArray(int arr[]) {
-        int n = arr.length;
-        for (int i = 0; i < n; ++i)
-            System.out.print(arr[i] + " ");
-        System.out.println();
-    }
-
-    // Driver code
-    public static void main(String args[]) {
-        int arr[] = { 12, 11, 13, 5, 6, 7 };
-
-        System.out.println("Given array is");
-        printArray(arr);
-
-        quickSort(arr);
-
-        System.out.println("\nSorted array is");
-        printArray(arr);
-    }
 }
--- parallel-based-sorting-algorithms/src/test/java/michaeljosh/quickSortTest.java
+++ parallel-based-sorting-algorithms/src/test/java/michaeljosh/quickSortTest.java
@@ -1,16 +1,17 @@
 package michaeljosh;
 
+import org.junit.Test;
+import static org.junit.Assert.assertArrayEquals;
+
 public class quickSortTest {
-        @Test
-        public void testMergeSort() {
-            int[] unsortedArray = { 4, 5, 6, 7, 3, 1, 8, 2, 9, 10 };
-            int[] sortedArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-    
-            mergeSort.sort(unsortedArray); // Assuming you have a Mergesort class with a static sort method
-    
-            // Assert that the sorted array matches the expected result
-            assertArrayEquals(sortedArray, unsortedArray);
-        }
+    @Test
+    public void testQuickSort() {
+        int[] unsortedArray = { 4, 5, 6, 7, 3, 1, 8, 2, 9, 10 };
+        int[] sortedArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        quickSort.quickSort(unsortedArray); // Assuming you have a Mergesort class with a static sort method
+
+        // Assert that the sorted array matches the expected result
+        assertArrayEquals(sortedArray, unsortedArray);
     }
-    
 }
