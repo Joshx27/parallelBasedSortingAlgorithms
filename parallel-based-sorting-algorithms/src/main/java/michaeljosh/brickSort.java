1d11125c4febda2b5a91af84cdfb38e2518de088
michael1018 <mmm831018@gmail.com>
2023-11-09 14:54:45 -0600

    Implement brickSort func

--- parallel-based-sorting-algorithms/src/main/java/michaeljosh/brickSort.java
+++ parallel-based-sorting-algorithms/src/main/java/michaeljosh/brickSort.java
@@ -1,5 +1,41 @@
 package michaeljosh;
 
-public class brickSort {
+import java.util.Arrays;
 
+public class brickSort {
+    public static void brickSort(int arr[]) {
+        int n = arr.length;
+        boolean isSorted = false; // Initially array is unsorted
+
+        while (!isSorted) {
+            isSorted = true;
+            int temp = 0;
+
+            // Perform Bubble sort on odd indexed element
+            for (int i = 1; i <= n - 2; i = i + 2) {
+                if (arr[i] > arr[i + 1]) {
+                    temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                    isSorted = false;
+                }
+            }
+
+            // Perform Bubble sort on even indexed element
+            for (int i = 0; i <= n - 2; i = i + 2) {
+                if (arr[i] > arr[i + 1]) {
+                    temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                    isSorted = false;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {34, 2, 10, -9};
+        brickSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
 }
--- parallel-based-sorting-algorithms/src/test/java/michaeljosh/brickSortTest.java
+++ parallel-based-sorting-algorithms/src/test/java/michaeljosh/brickSortTest.java
@@ -1,5 +1,31 @@
 package michaeljosh;
 
+import org.junit.Test;
+
+import static michaeljosh.brickSort.brickSort;
+import static org.junit.Assert.assertArrayEquals;
+
 public class brickSortTest {
+
+    @Test
+    public void test1() {
+        int a[] = {3, 7, 4, 8, 6, 2, 1, 5};
+        brickSort(a);
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7, 8}, a);
+    }
+
+    @Test
+    public void test2() {
+        int a[] = {5, 10, 15, 25, 20, 3, 2, 1};
+        brickSort(a);
+        assertArrayEquals(new int[]{1, 2, 3, 5, 10, 15, 20, 25}, a);
+    }
+
+    @Test
+    public void test3() {
+        int a[] = {3, 7, 9, 8, 6, 2, 1, 5};
+        brickSort(a);
+        assertArrayEquals(new int[]{1, 2, 3, 5, 6, 7, 8, 9}, a);
+    }
 
 }
