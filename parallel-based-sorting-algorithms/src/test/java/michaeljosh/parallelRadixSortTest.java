97590cb7df90a85497ae6fe980a4cbd2d14565ec
Joshx27 <joshuajhill2002@gmail.com>
2023-11-10 09:00:11 -0600

    radix sort DONEEEEEE

--- parallel-based-sorting-algorithms/src/test/java/michaeljosh/parallelRadixSortTest.java
+++ parallel-based-sorting-algorithms/src/test/java/michaeljosh/parallelRadixSortTest.java
@@ -1,5 +1,24 @@
 package michaeljosh;
 
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
 public class parallelRadixSortTest {
 
+    @Test
+    public void testRadixSort() {
+        // Create an instance of the parallelRadixSort class
+        parallelRadixSort sorter = new parallelRadixSort();
+
+        // Create an array for testing
+        int[] arrayToSort = { 4, 2, 7, 1, 9, 5, 3, 8, 6 };
+
+        // Call the radixMulti method to sort the array
+        int[] sortedArray = sorter.radixMulti(arrayToSort);
+
+        // Verify that the array is sorted
+        for (int i = 0; i < sortedArray.length - 1; i++) {
+            assertTrue(sortedArray[i] <= sortedArray[i + 1]);
+        }
+    }
 }
