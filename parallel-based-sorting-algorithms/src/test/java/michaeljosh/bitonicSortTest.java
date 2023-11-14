9e40d369f9f7436f83632997332c847dae25cfd4
michael1018 <mmm831018@gmail.com>
2023-11-14 14:00:09 -0600

    fix bitonicSort func

--- parallel-based-sorting-algorithms/src/test/java/michaeljosh/bitonicSortTest.java
+++ parallel-based-sorting-algorithms/src/test/java/michaeljosh/bitonicSortTest.java
@@ -3,7 +3,6 @@ package michaeljosh;
 import org.junit.Test;
 
 
-import static michaeljosh.bitonicSort.sort;
 import static org.junit.Assert.assertArrayEquals;
 
 
@@ -11,14 +10,16 @@ public class bitonicSortTest {
     @Test
     public void test1() {
         int a[] = {3, 7, 4, 8, 6, 2, 1, 5};
-        sort(a, a.length, 1);
+        bitonicSort bitonic = new bitonicSort();
+        bitonic.sort(a);
         assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7, 8}, a);
     }
 
     @Test
     public void test2() {
         int a[] = {5, 10, 15, 25, 20, 3, 2, 1};
-        sort(a, a.length, 1);
+        bitonicSort bitonic = new bitonicSort();
+        bitonic.sort(a);
         assertArrayEquals(new int[]{1, 2, 3, 5, 10, 15, 20, 25}, a);
     }
 
