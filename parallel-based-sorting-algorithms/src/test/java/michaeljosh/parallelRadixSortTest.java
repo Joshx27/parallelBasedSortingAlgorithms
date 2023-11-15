13c23ed2bb4e4d3cf5887827be42fe32407c20bc
Joshua Hill <98056388+Joshx27@users.noreply.github.com>
2023-11-15 12:07:10 -0600

    notsure

--- parallel-based-sorting-algorithms/src/test/java/michaeljosh/parallelRadixSortTest.java
+++ parallel-based-sorting-algorithms/src/test/java/michaeljosh/parallelRadixSortTest.java
@@ -1,24 +1,27 @@
-package michaeljosh;
+// package michaeljosh;
 
-import org.junit.Test;
-import static org.junit.Assert.assertTrue;
+// import org.junit.Test;
 
-public class parallelRadixSortTest {
+// import michaeljosh.RadixSort.ParallelRadixSort;
 
-    @Test
-    public void testRadixSort() {
-        // Create an instance of the parallelRadixSort class
-        parallelRadixSort sorter = new parallelRadixSort();
+// import static org.junit.Assert.assertTrue;
 
-        // Create an array for testing
-        int[] arrayToSort = { 4, 2, 7, 1, 9, 5, 3, 8, 6 };
+// public class parallelRadixSortTest {
 
-        // Call the radixMulti method to sort the array
-        int[] sortedArray = sorter.radixMulti(arrayToSort);
+// @Test
+// public void testRadixSort() {
+// // Create an instance of the parallelRadixSort class
+// ParallelRadixSort sorter = new ParallelRadixSort();
 
-        // Verify that the array is sorted
-        for (int i = 0; i < sortedArray.length - 1; i++) {
-            assertTrue(sortedArray[i] <= sortedArray[i + 1]);
-        }
-    }
-}
+// // Create an array for testing
+// int[] arrayToSort = { 4, 2, 7, 1, 9, 5, 3, 8, 6 };
+
+// // Call the radixMulti method to sort the array
+// int[] sortedArray = sorter.radixMulti(arrayToSort);
+
+// // Verify that the array is sorted
+// for (int i = 0; i < sortedArray.length - 1; i++) {
+// assertTrue(sortedArray[i] <= sortedArray[i + 1]);
+// }
+// }
+// }
